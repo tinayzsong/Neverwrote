f13679678d4cd50ff9bed008a742336ba51dace1
tinayzsong <tinayzsong@gmail.com>
2020-11-04 18:15:40 +0800

    update note components

--- frontend/src/components/NoteEdit.js
+++ frontend/src/components/NoteEdit.js
@@ -1,67 +1,64 @@
 const React = require('react');
+const MarkdownEditor = require('./MarkdownEditor');
 const _ = require('lodash');
 
-/**
- * A form for editing a note title.
- */
 class NoteEdit extends React.Component {
-  constructor(props) {
-    super(props);
-    const note = props.note || {};
-
-    this.state = {
-      title: note.title || '',
-
-    };
-  }
-
-  render() {
-    const revertAndStopEditing = (event) => {
-      event.preventDefault();
-      this.props.onCancel();
-    };
-
-    const submitAndStopEditing = (event) => {
-      event.preventDefault();
-      // Creates a new noebook object and saves it.
-      const editedNote = _.assign({}, this.props.note, {
-        title: this.state.title,
-
-      });
-      this.props.onSave(editedNote);
-    };
-
-    const onTitleChange = (event) => {
-      this.setState({ title: event.target.value });
-    };
-
-
-
-    return (
-      <form className="blog-post">
-        {/* Title field */}
-        <div className="form-group">
-          <input className="form-control input-lg" value={this.state.title}
-            placeholder="note title" onChange={onTitleChange}
-          />
-        </div>
-
-        {/* Save button */}
-        <button className="btn btn-primary pull-right"
-          onClick={submitAndStopEditing}
-        >
-          Save
-        </button>
-        {/* Cancel button */}
-        <button className="btn btn-danger pull-right"
-          style={{ marginRight: '12px' }}
-          onClick={revertAndStopEditing}
-        >
-          Cancel
-        </button>
-      </form>
-    );
-  }
+    constructor(props) {
+        super(props);
+        const note = props.note || {};
+        this.state = {
+            title: note.title || '',
+            content: note.content || '',
+            notebookId: props.notebookId || ''
+        };
+    }
+    render() {
+        const revertAndStopEditing = event => {
+            event.preventDefault();
+            this.props.onCancel();
+        };
+        const submitAndStopEditing = event => {
+            event.preventDefault();
+            const editedPost = _.assign({}, this.props.note, {
+                title: this.state.title,
+                content: this.state.content,
+                notebookId: this.props.notebookId
+            });
+            this.props.onSave(editedPost);
+            this.props.onCancel();
+        };
+        const onTitleChange = event => {
+            this.setState({ 
+                title: event.target.value 
+            });
+        };
+        const onContentChange = event => {
+            this.setState({ 
+                content: event.target.value 
+            });
+        };
+        return (
+            <form>
+                <div className="form-group">
+                    <input className="form-control" 
+                        value={this.state.title}
+                        placeholder="Title" 
+                        onChange={onTitleChange}
+                    />
+                    <MarkdownEditor value={this.state.content} 
+                        onChange={onContentChange} />
+                </div>
+                <button className="btn btn-danger pull-right btn-lg"
+                    onClick={revertAndStopEditing}
+                > ×
+                </button>
+                <button className="btn btn-success pull-right btn-lg"
+                    onClick={submitAndStopEditing}
+                > ✔️
+                </button>
+            </form>
+        );
+    }
 }
 
 module.exports = NoteEdit;
--- frontend/src/components/NoteNew.js
+++ frontend/src/components/NoteNew.js
@@ -1,54 +1,48 @@
 const React = require('react');
 const NoteEdit = require('./NoteEdit');
 
-/**
- * A button which expands into a form for writing a new note.
- */
 class NoteNew extends React.Component {
-  constructor(props) {
-    super(props);
-    // Set initial internal state for this component
-    this.state = { editing: false };
-  }
-
-  render() {
-    const openEdit = () => {
-      this.setState({ editing: true });
-    };
-
-    const closeEdit = () => {
-      this.setState({ editing: false });
-    };
-
-    const createNote = (newNote) => {
-      this.props.createNote(newNote, (err) => {
-        if(!err) closeEdit();
-      });
-    };
-
-    // to switch to edit mode when editing is clicked.
-       if(this.state.editing) {
-      // Render component for editing the note
-      return (
-        <NoteEdit
-          note={this.props.note}
-          onSave={createNote}
-          onCancel={closeEdit}
-        />
-
-      );
+    constructor(props) {
+        super(props);
+        this.state = {
+            editing: false,
+        };
     }
-
-
-
-    return (
-      <button className="blog-load-more btn btn-primary btn-lg"
-        onClick={ openEdit }
-      >
-        <span class="fa fa plus"></span>Add a new note
-      </button>
-    );
-  }
+    render() {
+        const openEdit = () => {
+            this.setState({ 
+                editing: true 
+            });
+        };
+        const closeEdit = () => {
+            this.setState({ 
+                editing: false 
+            });
+            this.props.clearInput();
+        }
+        const createNote = newNote => {
+            this.props.createNote(newNote, e => {
+                if (!e) {
+                    closeEdit();
+                }
+            })
+        };
+        if (this.state.editing) {
+            return (
+                <NoteEdit
+                    note={this.props.note}
+                    onSave={createNote}
+                    onCancel={closeEdit}
+                    notebookId={this.props.notebookId}
+                />
+            );
+        }
+        return (
+            <button className="btn btn-primary"
+                onClick={openEdit}
+            >＋Note
+            </button>
+        );
+    }
 }
-
 module.exports = NoteNew;
