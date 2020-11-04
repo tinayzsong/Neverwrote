93874b5b0690eab7b0172c0365b1797eeaaec8ef
tinayzsong <tinayzsong@gmail.com>
2020-11-04 18:14:59 +0800

    improve create new notebook component

--- frontend/src/components/NoteBookNew.js
+++ frontend/src/components/NoteBookNew.js
@@ -1,54 +1,46 @@
 const React = require('react');
 const NotebookEdit = require('./NotebookEdit');
 
-/**
- * A button which expands into a form for writing a new notebook.
- */
-class NoteBookNew extends React.Component {
+class NotebookNew extends React.Component{
   constructor(props) {
     super(props);
-    // Set initial internal state for this component
-    this.state = { editing: false };
+    this.state = {
+        editing:false
+    };
   }
-
   render() {
     const openEdit = () => {
-      this.setState({ editing: true });
+        this.setState({
+            editing: true
+        });
     };
-
     const closeEdit = () => {
-      this.setState({ editing: false });
+      this.setState({
+          editing: false
+        });
     };
-
-    const createNotebook = (newNotebook) => {
-      this.props.createNotebook(newNotebook, (err) => {
-        if(!err) closeEdit();
+    const createNotebook = newNotebook => {
+      this.props.createNotebook(newNotebook, e => {
+        if(!e) {
+            closeEdit();
+        }
       });
     };
-
-    // to switch to edit mode when editing is clicked.
-       if(this.state.editing) {
-      // Render component for editing the notebook
-      return (
+    
+    if(this.state.editing) {
+	  return (
         <NotebookEdit
-          notebook={this.props.notebook}
+          notebook = {this.props.notebook}
           onSave={createNotebook}
           onCancel={closeEdit}
         />
-
       );
     }
-
-
-
-    return (
-      <button className="blog-load-more btn btn-primary btn-lg"
-        onClick={ openEdit }
-      >
-        <span className="fa fa plus"></span>Add a new notebook
+    return(
+      <button className="btn btn-primary" 
+        onClick={openEdit}> Add Notebook
       </button>
     );
   }
 }
-
-module.exports = NoteBookNew;
+module.exports = NotebookNew;
